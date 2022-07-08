b85dee2156382c89fddb6f95b8ba0e36fe85fa61
DanishCoder <Danishim66@gmail.com>
2022-07-07 21:45:01 -0400

    final changes

--- client/main.js
+++ client/main.js
@@ -1,14 +1,21 @@
 const complimentBtn = document.getElementById("complimentButton")
 const fortuneBtn = document.getElementById("fortuneButton")
+const formElement = document.getElementById("powerForm")
+const nameInput = document.getElementById("name")
+const plInput = document.getElementById("pl")
+const formIncElement = document.getElementById("incForm")
+const nameIncInput = document.getElementById("nameInc")
+const formDelElement = document.getElementById("delForm")
+const nameDelInput = document.getElementById("nameDel")
 
 const baseURL = "http://localhost:4000"
-const getCompliment = () => {
+
+const getComplimentYowza = () => {
     axios.get(baseURL + "/api/compliment/")
         .then(res => {
             const data = res.data;
             alert(data);
     });
-    
 };
 
 const getFortune = () => {
@@ -16,11 +23,73 @@ const getFortune = () => {
         .then((res) => {
             alert(res.data)
         }).catch((error) => {
-            console.log(error)
+            alert('backend broken!')
         })
 }
 
+const submitPerson = (event) => {
+    event.preventDefault()
 
+    const name = nameInput.value
+    const pl = plInput.value
 
-complimentBtn.addEventListener('click', getCompliment)
+    const theBody = {
+        name: name,
+        power_level: +pl,
+    }
+
+    axios.post(baseURL + '/api/createUser', theBody)
+    .then((response) => {
+        if (response.data.success) {
+            console.log('new database looks like:')
+            console.log(response.data.info)
+        } else {
+            alert('failure')
+        }
+    })
+
+    nameInput.value = ''
+    plInput.value = ''
+    
+
+}
+
+const incPower = (event) => {
+    event.preventDefault()
+
+    const name = nameIncInput.value
+
+    axios.put('http://localhost:4000/api/incUser/' + name)
+    .then((response) => {
+        if (response.data.success) {
+            console.log('new database looks like:')
+            console.log(response.data.info)
+        } else {
+            alert('It failed!')
+        }
+    })
+  
+}
+
+const delUser = (event) => {
+    event.preventDefault()
+
+    const name = nameDelInput.value
+
+    axios.put('http://localhost:4000/api/delUser/' + name)
+    .then((response) => {
+        if (response.data.success) {
+            console.log('new database looks like:')
+            console.log(response.data.info)
+        } else {
+            alert('It failed!')
+        }
+    })
+  
+}
+
+complimentBtn.addEventListener('click', getComplimentYowza)
 fortuneBtn.addEventListener('click', getFortune)
+formElement.addEventListener('submit', submitPerson)
+formIncElement.addEventListener('submit', incPower)
+formDelElement.addEventListener('submit', delUser)
--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+        const compliments = ["People wish they were you", "Big brain", "You're gonna work at google soon! :)"];
       
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
@@ -11,17 +11,46 @@ module.exports = {
     },
     getFortune: (req, res) => {
         const fortunes = [
-        "You're going to die at 50",
+            "You're going to die at 50",
         "You'll win the lottery tomorrow",
         "Watch your back on September 23rd",
         "You'll see a color you've never seen before, tonight",
         "You'll assist the president with a problem in the near future",
         ]
-    let randomIndex = Math.floor(Math.random() * fortunes.length);
-    let randomFortune = fortunes[randomIndex];
 
-    res.status(200).send(randomFortune);
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+    createUser: (req, res) => {
+        database.push(req.body)
+        res.status(200).send({success: true, info: database})
+    },
+    incUser: (req, res) => {
+        const name = req.params.name
+        for (let i=0; i<database.length; i++) {
+            if (database[i].name === name) {
+                database[i].power_level++
+            }
+        }
+        res.status(200).send({success: true, info: database})
+    },
+    delUser: (req, res) => {
+        const name = req.params.name
+        for (let i=0; i<database.length; i++) {
+            if (database[i].name === name) {
+                database.splice(i, 1);
+            }
+        }
+        res.status(200).send({success: true, info: database})
     }
-    
 
 }
+
+const database = [
+    {
+        name:'Danish',
+        power_level:55
+    },
+]
